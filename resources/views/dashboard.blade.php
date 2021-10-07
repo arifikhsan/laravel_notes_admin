2eba5a29d83de1352d6462906d586f2c8572c557
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-10-08 02:31:25 +0700

    styling

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,7 +13,7 @@
                         <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
                             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                                 <table class="min-w-full divide-y divide-gray-200">
-                                    <thead class="bg-gray-50">
+                                    <thead class="bg-gray-100">
                                     <tr>
                                         <th scope="col"
                                             class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
@@ -31,7 +31,7 @@
                                     </thead>
                                     <tbody class="bg-white divide-y divide-gray-200">
                                     @foreach ($items as $item)
-                                        <tr>
+                                        <tr class="even:bg-gray-50">
                                             <td class="px-6 py-4 whitespace-nowrap">
                                                 <div class="flex items-center">
                                                     <a href="/dashboard/{{ $item['name'] }}">{{ $item['name'] }}</a>
